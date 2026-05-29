1ae8c07a18a0e25674a9b59335e944acb5d392c3
samprithasureshkumar-S <samprithasureshkumar@gmail.com>
2026-05-29 13:00:08 +0530

    Add files via upload

---
+++ Sampritha Task 3.py
@@ -0,0 +1,174 @@
+'''
+#task 1
+wallet=500
+wallet-=200
+print("Remaining Amount : ",wallet)
+'''
+'''
+#task 2
+salary=150000
+salary+=3000
+print("Updated Salary:",salary)
+'''
+'''
+#task 3
+tank=150
+tank+=50
+print("Total Water level:",tank)
+'''
+'''
+#task 4
+battery=90
+battery-=25
+print("Remaining Battery:",battery)
+'''
+'''
+#task 5
+savings=2000
+savings*=2
+print("Total Savings:",savings)
+'''
+'''
+#task 6
+bag=850
+bag+=150
+print("Final price:",bag)
+'''
+'''
+#task 7
+travel=120
+travel+=30
+print("Distance Travelled:",travel)
+'''
+'''
+#task 8
+score=95
+score-=15
+print("Total Score:",score)
+'''
+'''
+#task 9
+number=12
+number*=5
+print("Number:",number)
+'''
+'''
+#task 10
+chocolate=40
+chocolate-=10
+print("Remaining Chocolate:",chocolate)
+'''
+'''
+#task 11
+balance=15000
+balance-=5000
+print("Updated Balance:",balance)
+'''
+'''
+#task 12
+classroom=60
+classroom-=5
+print("Remaining Students:",classroom)
+'''
+'''
+#task 13
+salary=8000
+salary*=3
+print("Tripled Salary:",salary)
+'''
+'''
+#task 14
+price=950
+price+=50
+print("Final Price:",price)
+'''
+'''
+#task 15
+run=120
+run+=35
+print("Total Run:",run)
+'''
+'''
+#task 16
+battery=100
+battery-=40
+print("Total Battery:",battery)
+'''
+'''
+#task 17
+cows=25
+cows*=2
+print("Total Cows:",cows)
+'''
+'''
+#task 18
+pens=75
+pens-=20
+print("Total pens:",pens)
+'''
+'''
+#task 19
+mark=72
+mark+=8
+print("Total Marks:",mark)
+'''
+#task 20
+number=7
+number*=10
+print("Number:",number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
