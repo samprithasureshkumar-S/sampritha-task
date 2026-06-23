b599fc97c84217f162732bceb9b0e3508c20e348
samprithasureshkumar-S <samprithasureshkumar@gmail.com>
2026-06-23 10:49:12 +0530

    Add files via upload

---
+++ Debugging.py
@@ -0,0 +1,175 @@
+
+#task 1
+age = 18
+
+if age >=18:
+    print ('Eligible to Vote')
+
+#task 2
+num = 10
+
+if num==10:
+    print('Number is 10')
+
+#task 3
+marks = 75
+if marks >= 50:
+    print('Pass')
+
+#task 4
+num = -5
+
+if num > 0:
+    print('Positive')
+else:
+    print('Negative')
+
+#task 5
+temperature=30
+if temperature>35:
+    print('Hot')
+elif temperature>25:
+    print('Warm')
+else:
+    print('Cool')
+
+#task 6
+username = 'admin'
+
+if username == 'admin':
+    print('Login Success')
+
+#task 7
+number = 10
+
+if number > 5:
+    print('Greater')
+else:
+    print('Smaller')
+
+#task 8
+value = 100
+
+if value > 50:
+    print('High')
+else:
+    print('Low')
+
+#task 9
+for i in range(5):
+    print(i)
+
+#task 10
+for i in range (1,6):
+    print(i)
+
+#task 11
+i = 1
+
+while i <= 5:
+    print(i)
+    i+=1
+
+#task 12
+for i in range(5):
+    print(i)
+
+#task 13
+numbers = [10, 20, 30]
+
+for num in numbers:
+    print(num)
+
+#task 14
+for i in range(5):
+    print(i)
+    i += 1
+
+#task 15
+i = 1
+while i <= 5:
+    print(i)
+    i-=1
+
+#task 16
+for i in range(1, 11):
+    if i == 5:
+        print('Found')
+
+#task 17
+name = 'Python'
+print(name[4])
+
+#task 18
+text = 'hello'
+print(text.upper())
+
+#task 19
+message = 'Welcome'
+print(message.lower())
+
+#task 20
+word = 'Python'
+word='J'+word[1:]
+print(word)
+
+#task 21
+name = 'Praveen Kumar'
+print(name.split())
+
+#task 22
+text = 'Python'
+for i in range(len(text)):
+    print(text[i])
+
+#task 23
+language = 'Python'
+print(language + str(100))
+
+#task 24
+numbers = [10, 20, 30]
+print(numbers[1])
+
+#task 25
+numbers = [1, 2, 3]
+numbers.append(4)
+print(numbers)
+
+#task 26
+fruits = ['Apple', 'Orange']
+fruits.append('Banana')
+print(fruits)
+
+#task 27
+numbers = [10, 20, 30]
+numbers.remove(30)
+print(numbers)
+
+#task 28
+numbers = [10, 20, 30]
+print(len(numbers))
+
+#task 29
+names = ['A', 'B', 'C']
+for i in range(len(names)):
+    print(names[i])
+
+#task 30
+numbers = [10, 20, 30]
+total = 0
+for num in numbers:
+    total = total + num
+
+print(total)
+
+
+
+
+
+
+
+
+
+
+
+
